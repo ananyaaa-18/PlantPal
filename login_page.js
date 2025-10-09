378cb1c02ab8dbe2e2bd492bd138d5b465f53948
ananyaaa-18 <ananyaarana.18@gmail.com>
2025-10-09 18:24:13 +0530

    -refined the style of the 'login' page
    -added & designed inputs for both email id and password as well
    
    -refined the padding & margin layout nicely.

--- login_page.js
+++ login_page.js
@@ -1,4 +1,25 @@
+import { initializeApp } from "firebase/app";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+// Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyAuHjQZohof2eD-0D-cibetgXoVNlX0TQA",
+  authDomain: "plantpal-a552e.firebaseapp.com",
+  projectId: "plantpal-a552e",
+  storageBucket: "plantpal-a552e.firebasestorage.app",
+  messagingSenderId: "44877891019",
+  appId: "1:44877891019:web:15848735c784b7b5c0224e",
+  measurementId: "G-9S8EXT31ZT"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+export { auth, db };
+
 function logout()
 {
   window.location = "index.html";
-}
+};
